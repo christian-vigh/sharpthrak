4e627f883e2a144068dac98dc9950aa86f96bb94
Christian Vigh <christian.vigh@orange.fr>
2016-01-10 11:25:53 +0100

    Added EnvDTE objects extension methods

---
+++ Sources/DTE/EditPoint.cs
@@ -0,0 +1,105 @@
+﻿/**************************************************************************************************************
+
+    NAME
+	EditPoint.cs
+
+    DESCRIPTION
+	Extension methods for DTE.EditPoint.
+
+    AUTHOR
+	Christian Vigh, 01/2016.
+
+    HISTORY
+	[Version : 1.0]		[Date : 2016/01/09]     [Author : CV]
+		Initial version.
+
+ **************************************************************************************************************/
+using	System ;
+using	EnvDTE ;
+
+using	Thrak. Types ;
+
+
+namespace Thrak. EnvDTE
+   {
+	public static class EditPointExtensions
+	   {
+		/// <summary>
+		/// Creates an edit point for the specified line and column.
+		/// </summary>
+		public static EditPoint  CreateEditPoint ( this EditPoint  ep, int  line, int  column )
+		   {
+			EditPoint	new_ep		=  ep. CreateEditPoint ( ) ;
+
+			new_ep. MoveToLineAndOffset ( line, column ) ;
+
+			return ( new_ep ) ;
+		    }
+		    
+
+		/// <summary>
+		/// Creates an edit point to the start of line position of the specified point.
+		/// </summary>
+		public static EditPoint	 CreateBolEditPoint ( this EditPoint  ep )
+		   {
+			EditPoint	new_ep		=  ep. CreateEditPoint ( ) ;
+
+			new_ep. StartOfLine ( ) ;
+
+			return ( new_ep ) ;
+		    }
+
+
+		/// <summary>
+		/// Creates an edit point to the end of line position of the specified point.
+		/// </summary>
+		public static EditPoint  CreateEolEditPoint ( this EditPoint  ep )
+		   {
+			EditPoint	new_ep		=  ep. CreateEditPoint ( ) ;
+
+			new_ep. EndOfLine ( ) ;
+
+			return ( new_ep ) ;
+		    }
+		    				
+
+		/// <summary>
+		/// Creates an edit point to the first non-space character of a line.
+		/// </summary>
+		public static EditPoint	 FindFirstNonSpace ( this EditPoint  ep )
+		   {
+			if  ( ep. LineLength  ==  0 )
+				return ( null ) ;
+
+			EditPoint	new_ep		=  ep. CreateBolEditPoint ( ) ;
+			string		line		=  ep. GetText ( ep. LineLength ) ;
+			int		nonspace_index	=  0 ;
+
+			for  ( int  i = 0 ; i  <  line. Length ; i ++ )
+			   {
+				if  ( line [i]. IsSpace ( ) )
+					nonspace_index ++ ;
+				else
+					break ;
+			    }
+
+			new_ep. MoveToLineAndOffset ( new_ep. Line, nonspace_index + 1 ) ;
+
+			return ( new_ep ) ;
+		    }
+
+
+		/// <summary>
+		/// Gets a string representing the leding spaces of the specified line.
+		/// </summary>
+		public static string  GetLeadingSpaces ( this EditPoint  ep )
+		   {
+			EditPoint	new_ep	=  ep. FindFirstNonSpace ( ) ;
+
+			if  ( new_ep  ==  null  ||  new_ep. LineCharOffset  ==  1 )
+				return ( "" ) ;
+			else
+				return ( ep. GetText ( new_ep. LineCharOffset - 1 ) ) ;
+		    }
+	    }
+    }
---
+++ Sources/DTE/TextSelection.cs
@@ -0,0 +1,55 @@
+﻿/**************************************************************************************************************
+
+    NAME
+	TextSelection.cs
+
+    DESCRIPTION
+	Extension methods for DTE.Selection.
+
+    AUTHOR
+	Christian Vigh, 01/2016.
+
+    HISTORY
+	[Version : 1.0]		[Date : 2016/01/09]     [Author : CV]
+		Initial version.
+
+ **************************************************************************************************************/
+using	System ;
+using	EnvDTE ;
+
+using	Thrak. Types ;
+
+
+namespace Thrak. EnvDTE
+   {
+	public static class TextSelectionExtensions
+	   {
+		/// <summary>
+		/// Returns the active point in the selection.
+		/// </summary>
+		public static EditPoint  GetActivePoint ( this TextSelection  selection )
+		   { return ( ( EditPoint ) selection. ActivePoint. CreateEditPoint ( ) ) ; }
+
+
+		/// <summary>
+		/// Returns the anchor point.
+		/// </summary>
+		public static EditPoint  GetAnchorPoint ( this TextSelection  selection )
+		   { return ( ( EditPoint ) selection. AnchorPoint. CreateEditPoint ( ) ) ; }
+
+
+		/// <summary>
+		/// Returns the selection's bottom point.
+		/// </summary>
+		public static EditPoint  GetBottomPoint ( this TextSelection  selection )
+		   { return ( ( EditPoint ) selection. BottomPoint. CreateEditPoint ( ) ) ; }
+
+
+		/// <summary>
+		/// Returns the selection start (top) point.
+		/// </summary>
+		public static EditPoint  GetTopPoint ( this TextSelection  selection )
+		   { return ( ( EditPoint ) selection. TopPoint. CreateEditPoint ( ) ) ; }
+		   
+	    }
+    }
